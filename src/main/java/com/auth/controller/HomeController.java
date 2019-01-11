a6a77ab06b188d4af68b01535eb6a461b195ad01
leeyunjea <yunjea0312@naver.com>
2019-01-11 16:47:21 +0900

    Update RedisConfig

--- src/main/java/com/auth/controller/HomeController.java
+++ src/main/java/com/auth/controller/HomeController.java
@@ -1,6 +1,12 @@
 package com.auth.controller;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,18 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class HomeController {
+	
+	@Resource(name="redisTemplate")
+	private HashOperations<String, String, String> hashOperations;
 
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public ResponseEntity<String> home(@RequestBody String token) {
 		
 		//토큰이 있으면 redis로 검증 
 		System.out.println("API Home " + token);
+		token = token.replace("=", "").replace('/', '_').replace('+', '-');
+		System.out.println("API Home " + token);
+		// test -> 조회해보기 
+		String userId = hashOperations.get(token, "userId");
+		String ip = hashOperations.get(token, "ip");
+		String timeStamp = hashOperations.get(token, "timeStamp");
 		
-		//토큰이 없으면 API로 들어올 수 없음 error메세지 보내기 
+		System.out.println(userId + " " + ip + " " + timeStamp);
 		
-		String helloWorld = "HelloWorld";
-
-		return new ResponseEntity<String>(helloWorld, HttpStatus.OK);
-//		return new ResponseEntity<User>(HttpStatus.FORBIDDEN);
+		String helloWorld = "";
+		
+		if(userId != null && ip != null && timeStamp !=null) {
+			helloWorld = "HelloWorld";
+			
+			return new ResponseEntity<String>(helloWorld, HttpStatus.OK);
+		}else { //토큰이 없으면 API로 들어올 수 없음 error메세지 보내기 
+			helloWorld = "error";
+			return new ResponseEntity<String>(helloWorld, HttpStatus.OK);
+		}
+		
+		
 	}
 }
